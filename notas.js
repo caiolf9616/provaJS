dd5028af8a6bf3977085dd30735f1731fad68ce0
Caio Fernandes <133905605+caiolf9616@users.noreply.github.com>
2024-05-25 13:45:31 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,13 @@
+let nota1 = 8.0
+
+let nota2 = 7.5
+
+let nota3 = 9.4
+
+let peso1 = 3
+let peso2 = 2 
+let peso3 = 5 
+
+const mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3)
+
+console.log(mediaPonderada)
